dab489dc0481a7781db3cf6fc5eab9eab961568e
Zakaria <z.mansouri_etu@enset-media.ac.ma>
2023-03-05 06:21:03 +0100

    add instructions for defining the cluster and the node pool

--- 02 - Kuberntes - Cluster/infra.tf
+++ 02 - Kuberntes - Cluster/infra.tf
@@ -102,3 +102,60 @@ resource "oci_core_subnet" "vcn_public_subnet" {
   security_list_ids = [oci_core_security_list.public_subnet_sl.id]
   display_name      = "k8s-public-subnet"
 }
+
+
+# Kubernetes cluster
+resource "oci_containerengine_cluster" "k8s_cluster" {
+  compartment_id     = var.compartment_id
+  kubernetes_version = "v1.25.4"
+  name               = "zakaria-cluster"
+  vcn_id             = module.vcn.vcn_id
+
+  endpoint_config {
+    is_public_ip_enabled = true
+    subnet_id            = oci_core_subnet.vcn_public_subnet.id
+  }
+
+  options {
+    add_ons {
+      is_kubernetes_dashboard_enabled = false
+      is_tiller_enabled               = false
+    }
+    kubernetes_network_config {
+      pods_cidr     = "10.244.0.0/16"
+      services_cidr = "10.96.0.0/16"
+    }
+    service_lb_subnet_ids = [oci_core_subnet.vcn_public_subnet.id]
+  }
+}
+data "oci_identity_availability_domains" "ads" {
+  compartment_id = var.compartment_id
+}
+
+resource "oci_containerengine_node_pool" "k8s_node_pool" {
+  cluster_id         = oci_containerengine_cluster.k8s_cluster.id
+  compartment_id     = var.compartment_id
+  kubernetes_version = "v1.25.4"
+  name               = "k8s-node-pool"
+  node_config_details {
+    placement_configs {
+      availability_domain = data.oci_identity_availability_domains.ads.availability_domains[0].name
+      subnet_id           = oci_core_subnet.vcn_private_subnet.id
+    }
+    size = 2
+  }
+  node_shape = "VM.Standard.A1.Flex"
+  node_shape_config {
+    memory_in_gbs = 6
+    ocpus         = 1
+  }
+  node_source_details {
+    image_id    = "	ocid1.image.oc1.eu-paris-1.aaaaaaaagbpwjqiwwzybgsrh6tz3rxi3hzkqx6aswmijqdvspdmybuxd4qkq"
+    source_type = "image"
+  }
+  initial_node_labels {
+    key   = "name"
+    value = "k8s-cluster"
+  }
+  ssh_public_key = var.ssh_key
+}
