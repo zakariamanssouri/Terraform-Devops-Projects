59c0d1a2e3e483e97ff435c794a99228da1ddf14
Zakaria <z.mansouri_etu@enset-media.ac.ma>
2023-03-05 06:03:19 +0100

    add instructions for adding public and private subnets with security lists

--- 02 - Kuberntes - Cluster/infra.tf
+++ 02 - Kuberntes - Cluster/infra.tf
@@ -33,3 +33,72 @@ module "vcn" {
   create_nat_gateway      = true
   create_service_gateway  = true
 }
+
+
+resource "oci_core_security_list" "private_subnet_sl" {
+  compartment_id = var.compartment_id
+  vcn_id         = module.vcn.vcn_id
+  display_name   = "k8s-private-subnet-sl"
+
+  egress_security_rules {
+    stateless        = false
+    destination      = "0.0.0.0/0"
+    destination_type = "CIDR_BLOCK"
+    protocol         = "all"
+  }
+
+  ingress_security_rules {
+    stateless   = false
+    source      = "10.0.0.0/16"
+    source_type = "CIDR_BLOCK"
+    protocol    = "all"
+  }
+
+}
+
+resource "oci_core_security_list" "public_subnet_sl" {
+  compartment_id = var.compartment_id
+  vcn_id         = module.vcn.vcn_id
+  display_name   = "k8s-public-subnet-sl"
+  egress_security_rules {
+    stateless        = false
+    destination      = "0.0.0.0/0"
+    destination_type = "CIDR_BLOCK"
+    protocol         = "all"
+  }
+  ingress_security_rules {
+    stateless   = false
+    source      = "10.0.0.0/16"
+    source_type = "CIDR_BLOCK"
+    protocol    = "all"
+  }
+  ingress_security_rules {
+    stateless   = false
+    source      = "0.0.0.0/0"
+    source_type = "CIDR_BLOCK"
+    protocol    = "6"
+    tcp_options {
+      min = 6443
+      max = 6443
+    }
+  }
+}
+
+resource "oci_core_subnet" "vcn_private_subnet" {
+  compartment_id = var.compartment_id
+  vcn_id         = module.vcn.vcn_id
+  cidr_block     = "10.0.1.0/24"
+
+  route_table_id    = module.vcn.nat_route_id
+  security_list_ids = [oci_core_security_list.private_subnet_sl.id]
+  display_name      = "k8s-private-subnet"
+}
+
+resource "oci_core_subnet" "vcn_public_subnet" {
+  compartment_id    = var.compartment_id
+  vcn_id            = module.vcn.vcn_id
+  cidr_block        = "10.0.0.0/24"
+  route_table_id    = module.vcn.ig_route_id
+  security_list_ids = [oci_core_security_list.public_subnet_sl.id]
+  display_name      = "k8s-public-subnet"
+}
